9b3cf2a46e0f2f02f047420b9c939cdfd9cca7fe
RyoyaFukushima <101489172+RyoyaFukushima@users.noreply.github.com>
2022-05-23 18:02:41 +0900

    fix comment

--- openWeatherAPI.php
+++ openWeatherAPI.php
@@ -1,6 +1,6 @@
 <?php
 // OpenWeatherAPIを使って現在の仙台の天気を取得してみる
-
+// API認証がまだ通らないので後程修正したい。
 $testurl = 'https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=54a2e41241a1040e54ef1f234b60a008';
 $res = file_get_contents($testurl);
 $weather = json_decode($res, true);
