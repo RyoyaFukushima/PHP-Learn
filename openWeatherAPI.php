daee4b556cd8e92e17b6382380f9cf9050c5e24c
RyoyaFukushima <101489172+RyoyaFukushima@users.noreply.github.com>
2022-05-24 09:33:35 +0900

    fix 401 error

--- openWeatherAPI.php
+++ openWeatherAPI.php
@@ -1,9 +1,23 @@
 <?php
 // OpenWeatherAPIを使って現在の仙台の天気を取得してみる
-// API認証がまだ通らないので後程修正したい。
-$testurl = 'https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=54a2e41241a1040e54ef1f234b60a008';
-$res = file_get_contents($testurl);
-$weather = json_decode($res, true);
+// TODO:GitHubに上げる場合はAPIKEYを隠す
+$url = 'https://api.openweathermap.org/data/2.5/weather?q=Sendai&units=metric&APPID=APIKEY';
+$res = file_get_contents($url);
+$res_json = json_decode($res, true);
+// 天気部分をパースして取得
+$weather = $res_json['weather'][0]['main'];
+// 気温部分をパースして取得
+$temp = $res_json['main']['temp'];
+// 英語の結果を日本語に簡易的に変換
+$jpweather = match($weather){
+    'Clear' => '晴れ',
+    'Clouds' => '曇り',
+    'Rain' => '雨',
+    'Snow' => '雪'
+};
 
-echo $weather;
+echo '簡易天気表示';
+echo '<br>';
+echo '現在の仙台の天気は' . $jpweather . '。';
+echo '気温は'. strval($temp) .'度です。';
 ?>
